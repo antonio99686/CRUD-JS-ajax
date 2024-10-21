1b847e1f448e0362fc48f5176b737e1fd5d5f634
antonio99686 <antonio.2022324018@aluno.iffar.edu.br>
2024-10-21 11:24:17 -0300

    arrumando

--- script.js
+++ script.js
@@ -15,39 +15,55 @@ function listarTodos() {
 }
 
 function inserircarro(carro) {
-    for (const usuario of carro) {
-        inserirUsuario(usuario);
+    for (const carro of carro) {
+        inserircarro(carro);
     }
 }
 
-function inserirUsuario(usuario) {
+function inserircarro(carro) {
     let tbody = document.getElementById('carro');
     let tr = document.createElement('tr');
+    
     let tdId = document.createElement('td');
-    tdId.innerHTML = usuario.id_carro;
-    let tdmodelo = document.createElement('td');
-    tdmodelo.innerHTML = usuario.modelo;
-    let tdmarca = document.createElement('td');
-    tdmarca.innerHTML = usuario.marca;
+    tdId.innerHTML = carro.id_carro;
+    
+    let tdModelo = document.createElement('td');
+    tdModelo.innerHTML = carro.modelo;
+    
+    let tdMarca = document.createElement('td');
+    tdMarca.innerHTML = carro.marca;
+    
+    let tdAno = document.createElement('td');
+    tdAno.innerHTML = carro.ano; // Added ano
+    
+    let tdPreco = document.createElement('td');
+    tdPreco.innerHTML = carro.preco; // Added preco
+    
     let tdAlterar = document.createElement('td');
     let btnAlterar = document.createElement('button');
     btnAlterar.innerHTML = "Alterar";
-    btnAlterar.addEventListener("click", buscaUsuario, false);
-    btnAlterar.id_carro = usuario.id_carro;
+    btnAlterar.addEventListener("click", buscacarro, false);
+    btnAlterar.id_carro = carro.id_carro;
     tdAlterar.appendChild(btnAlterar);
+    
     let tdExcluir = document.createElement('td');
     let btnExcluir = document.createElement('button');
     btnExcluir.addEventListener("click", excluir, false);
-    btnExcluir.id_carro = usuario.id_carro;
+    btnExcluir.id_carro = carro.id_carro;
     btnExcluir.innerHTML = "Excluir";
     tdExcluir.appendChild(btnExcluir);
+    
     tr.appendChild(tdId);
-    tr.appendChild(tdmodelo);
-    tr.appendChild(tdmarca);
+    tr.appendChild(tdModelo);
+    tr.appendChild(tdMarca);
+    tr.appendChild(tdAno); // Append ano
+    tr.appendChild(tdPreco); // Append preco
     tr.appendChild(tdAlterar);
     tr.appendChild(tdExcluir);
+    
     tbody.appendChild(tr);
 }
+
 
 function excluir(evt) {
     let id_carro = evt.currentTarget.id_carro;
@@ -60,12 +76,12 @@ function excluir(evt) {
             }
         )
             .then(response => response.json())
-            .then(retorno => excluirUsuario(retorno, id_carro))
+            .then(retorno => excluircarro(retorno, id_carro))
             .catch(error => console.log(error));
     }
 }
 
-function excluirUsuario(retorno, id_carro) {
+function excluircarro(retorno, id_carro) {
     if (retorno == true) {
         let tbody = document.getElementById('carro');
         for (const tr of tbody.children) {
@@ -76,62 +92,82 @@ function excluirUsuario(retorno, id_carro) {
     }
 }
 
-function alterarUsuario(usuario) {
+function alterarcarro(carro) {
     let tbody = document.getElementById('carro');
     for (const tr of tbody.children) {
-        if (tr.children[0].innerHTML == usuario.id_carro) {
-            tr.children[1].innerHTML = usuario.modelo;
-            tr.children[2].innerHTML = usuario.marca;
+        if (tr.children[0].innerHTML == carro.id_carro) {
+            tr.children[1].innerHTML = carro.modelo;
+            tr.children[2].innerHTML = carro.marca;
+            tr.children[3].innerHTML = carro.ano;   // Update ano
+            tr.children[4].innerHTML = carro.preco; // Update preco
         }
     }
 }
 
-function buscaUsuario(evt) {
+
+function buscacarro(evt) {
     let id_carro = evt.currentTarget.id_carro;
-    fetch('buscaUsuario.php?id_carro=' + id_carro,
+    fetch('buscacarro.php?id_carro=' + id_carro,
         {
             method: "GET",
             headers: { 'Content-Type': "application/json; charset=UTF-8" }
         }
     )
         .then(response => response.json())
-        .then(usuario => preencheForm(usuario))
+        .then(carro => preencheForm(carro))
         .catch(error => console.log(error));
 }
 
-function preencheForm(usuario) {
-    let inputIDUsuario = document.getElementsByName("id_carro")[0];
-    inputIDUsuario.value = usuario.id_carro;
-    let inputmodelo = document.getElementsByName("modelo")[0];
-    inputmodelo.value = usuario.modelo
-    let inputmarca = document.getElementsByName("marca")[0];
-    inputmarca.value = usuario.marca;
+function preencheForm(carro) {
+    let inputIDcarro = document.getElementsByName("id_carro")[0];
+    inputIDcarro.value = carro.id_carro;
+    
+    let inputModelo = document.getElementsByName("modelo")[0];
+    inputModelo.value = carro.modelo;
+    
+    let inputMarca = document.getElementsByName("marca")[0];
+    inputMarca.value = carro.marca;
+    
+    let inputAno = document.getElementsByName("ano")[0]; // Added ano input
+    inputAno.value = carro.ano;
+    
+    let inputPreco = document.getElementsByName("preco")[0]; // Added preco input
+    inputPreco.value = carro.preco;
 }
 
-function salvarUsuario(event) {
-    // parar o comportamento padrão do form
-    event.preventDefault();
-    // obtém o input id_carro
-    let inputIDUsuario = document.getElementsByName("id_carro")[0];
-    // pega o valor do input id_carro
-    let id_carro = inputIDUsuario.value;
 
-    let inputmodelo = document.getElementsByName("modelo")[0];
-    let modelo = inputmodelo.value;
-    let inputmarca = document.getElementsByName("marca")[0];
-    let marca = inputmarca.value;
+function salvarcarro(event) {
+    event.preventDefault();
+    
+    let inputIDcarro = document.getElementsByName("id_carro")[0];
+    let id_carro = inputIDcarro.value;
+
+    let inputModelo = document.getElementsByName("modelo")[0];
+    let modelo = inputModelo.value;
+
+    let inputMarca = document.getElementsByName("marca")[0];
+    let marca = inputMarca.value;
+
+    let inputAno = document.getElementsByName("ano")[0]; // Added ano input
+    let ano = inputAno.value;
+
+    let inputPreco = document.getElementsByName("preco")[0]; // Added preco input
+    let preco = inputPreco.value;
+
     let inputSenha = document.getElementsByName("senha")[0];
     let senha = inputSenha.value;
 
     if (id_carro == "") {
-        cadastrar(id_carro, modelo, marca, senha);
+        cadastrar(id_carro, modelo, marca, ano, preco, senha);
     } else {
-        alterar(id_carro, modelo, marca, senha);
+        alterar(id_carro, modelo, marca, ano, preco, senha);
     }
     document.getElementsByTagName('form')[0].reset();
 }
 
-function cadastrar(id_carro, modelo, marca, senha) {
+
+
+function cadastrar(id_carro, modelo, marca, ano, preco, senha) {
     fetch('inserir.php',
         {
             method: 'POST',
@@ -139,17 +175,19 @@ function cadastrar(id_carro, modelo, marca, senha) {
                 id_carro: id_carro,
                 modelo: modelo,
                 marca: marca,
+                ano: ano,       // Added ano
+                preco: preco,   // Added preco
                 senha: senha
             }),
             headers: { 'Content-Type': "application/json; charset=UTF-8" }
         }
     )
-        .then(response => response.json())
-        .then(usuario => inserirUsuario(usuario))
-        .catch(error => console.log(error));
+    .then(response => response.json())
+    .then(carro => inserircarro(carro))
+    .catch(error => console.log(error));
 }
 
-function alterar(id_carro, modelo, marca, senha) {
+function alterar(id_carro, modelo, marca, ano, preco, senha) {
     fetch('alterar.php',
         {
             method: 'POST',
@@ -157,12 +195,14 @@ function alterar(id_carro, modelo, marca, senha) {
                 id_carro: id_carro,
                 modelo: modelo,
                 marca: marca,
+                ano: ano,       // Added ano
+                preco: preco,   // Added preco
                 senha: senha
             }),
             headers: { 'Content-Type': "application/json; charset=UTF-8" }
         }
     )
-        .then(response => response.json())
-        .then(usuario => alterarUsuario(usuario))
-        .catch(error => console.log(error));
+    .then(response => response.json())
+    .then(carro => alterarcarro(carro))
+    .catch(error => console.log(error));
 }
