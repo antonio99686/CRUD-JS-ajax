de935b27c2ac315f31099e9359e93177547dcdbc
antonio99686 <antonio.2022324018@aluno.iffar.edu.br>
2024-10-24 16:48:17 -0300

    arrumando

--- alterar.php
+++ alterar.php
@@ -5,9 +5,10 @@ $conexao = conectar();
 
 $carro = json_decode(file_get_contents("php://input"));
 $sql = "UPDATE carros SET
-        nome='$carro->nome', 
-        email='$carro->email', 
-        senha='$carro->senha'
+        modelo='$carro->modelo', 
+        marca='$carro->marca', 
+        ano='$carro->ano',
+        preco='$carro->preco'
         WHERE id_carro=$carro->id_carro";
 
 executarSQL($conexao, $sql);
