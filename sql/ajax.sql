c4b8f9a3398e338784abc9ac8236861ceec8ee63
antonio99686 <antonio.2022324018@aluno.iffar.edu.br>
2024-10-31 10:15:38 -0300

    sql

--- sql/ajax.sql
+++ sql/ajax.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 24-Out-2024 às 19:12
+-- Tempo de geração: 31-Out-2024 às 13:14
 -- Versão do servidor: 8.0.31
 -- versão do PHP: 8.0.26
 
@@ -29,21 +29,28 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `carros`;
 CREATE TABLE IF NOT EXISTS `carros` (
-  `id_carro` int NOT NULL AUTO_INCREMENT,
+  `id_veiculo` int NOT NULL AUTO_INCREMENT,
   `modelo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `marca` varchar(255) NOT NULL,
   `ano` date NOT NULL,
   `preco` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  PRIMARY KEY (`id_carro`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id_veiculo`)
+) ENGINE=MyISAM AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Extraindo dados da tabela `carros`
 --
 
-INSERT INTO `carros` (`id_carro`, `modelo`, `marca`, `ano`, `preco`) VALUES
-(1, 'quadrado', 'gol', '2006-01-14', '5000.00'),
-(2, 'quadrado', 'gol', '2006-05-10', '5865.66');
+INSERT INTO `carros` (`id_veiculo`, `modelo`, `marca`, `ano`, `preco`) VALUES
+(10, 'Kick', 'Nissan', '5555-05-22', '85000'),
+(9, 'Celerio', 'Suzuki', '0000-00-00', '55000'),
+(8, '3', 'BMW', '0000-00-00', '120000'),
+(7, 'A4', 'Audi', '0000-00-00', '150000'),
+(6, 'Fusca', 'Volkswagen', '0000-00-00', '30000'),
+(5, 'Gol', 'Volkswagen', '0000-00-00', '65000'),
+(4, 'Onix', 'Chevrolet', '0000-00-00', '70000'),
+(3, 'Focus', 'Ford', '0000-00-00', '80000'),
+(2, 'Civic', 'Honda', '0000-00-00', '95000');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
